19dae46a11fb47ef2a4af988a5a13ed10b6664b0
qelvyn99 <132350650+qelvyn99@users.noreply.github.com>
2024-06-27 16:15:57 +0000

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,29 @@
-i
+#!/usr/bin/python3
+
+"""
+Module to implement a pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function to return a list of lists of integers representing the
+    Pascal’s triangle of n
+
+    Args:
+        n (int): The number of rows to generate for Pascal's Triangle
+
+    Returns:
+        List[List[int]]: List of lists representing Pascal’s Triangle
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
